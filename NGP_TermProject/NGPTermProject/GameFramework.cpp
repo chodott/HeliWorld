b998dc8e8591e3af5b7298fcf5792fafaabdc92d
ChoDot <chohs990514@gmail.com>
2022-12-11 21:59:19 +0900

    fix HP

--- NGP_TermProject/NGPTermProject/GameFramework.cpp
+++ NGP_TermProject/NGPTermProject/GameFramework.cpp
@@ -527,15 +527,15 @@ void CGameFramework::AnimateObjects()
 				m_pPlayer->Animate(fTimeElapsed, NULL, &client->playerData[i]);	//player update
 				m_pScene->m_ppShaders[0]->m_ppObjects[i]->SetActive(false);
 				cout << client->playerStatus[i].playerHP << endl;
-				for (int i = 1; i < 11; ++i)
+				for (int j = 1; j < 11; ++j)
 				{
-					if (i <=(client->playerStatus[i].playerHP) / 10)
+					if (j <=(client->playerStatus[i].playerHP) / 10)
 					{
-						m_pScene->m_ppShaders[3]->m_ppObjects[i]->SetActive(true);
+						m_pScene->m_ppShaders[3]->m_ppObjects[j]->SetActive(true);
 					}
 					else
 					{
-						m_pScene->m_ppShaders[3]->m_ppObjects[i]->SetActive(false);
+						m_pScene->m_ppShaders[3]->m_ppObjects[j]->SetActive(false);
 					}
 				}
 			}
